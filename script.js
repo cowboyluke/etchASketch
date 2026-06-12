9cea9ec5b642570c2459970244364df145923c35
cowboyluke <189388733+cowboyluke@users.noreply.github.com>
2026-06-12 15:32:55 -0700

    Finish entire project on all guidelines. Additionally completed both bonus objectives.

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
 //references the needed div and then makes 16*16 divs inside it
 const container = document.querySelector("#container");
-for (i=0; i<(16*16);i++){
+const createNew = document.querySelector("button#newArt");
+
+let size = 16;
+
+function createGrid(blocks) { 
+for (i=0; i<(blocks*blocks);i++){
     let square = document.createElement("div");
     square.classList.add("square");
     container.appendChild(square);
-    square.addEventListener("mouseover", () => square.classList.add("hovered"));
-    //square.addEventListener("mouseout", () => {setTimeout(() => square.classList.remove("hovered"), 750)}); //makes a temporary trail
+    square.dataset.lightness = "100";
+    square.addEventListener("mouseover", () => {
+        colourRandomly(square);
+    });
+    square.style.flexBasis = ((100*(1/blocks)) + "%");
+}
+}
+
+createNew.addEventListener("click", () => {
+    do {
+    size = Number(prompt("Please enter the square dimension you would like:", 16));
+    if (!(Number.isInteger(size) == true && size > 0 && size <= 100)) {
+        alert("Please enter an integer between 1 and 100");
+    }
+    } while (!(Number.isInteger(size) == true && size > 0 && size <= 100));
+    if (container && container.children.length > 0) {
+        container.replaceChildren();
+    }
+    createGrid(size);
+})
+
+createGrid(16);
+
+function colourRandomly(cssSelector) {
+    cssSelector.dataset.hue = Math.floor(Math.random() * 360);
+    let currentDarkness = Number(cssSelector.dataset.lightness);
+    let currentStep = (100 - currentDarkness)/10;
+    if (currentStep <= 10) {
+        let newDarknessInt = currentDarkness - 10;
+        cssSelector.dataset.lightness = String(newDarknessInt);
+    }
+    cssSelector.style.backgroundColor = `hsl(${cssSelector.dataset.hue}, 100%, ${cssSelector.dataset.lightness}%)`;
 }
